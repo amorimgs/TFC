a13d5d4ed9b081d3b6940d738bf305ad1ea227f5
Gabriel Amorim <ricksamann@gmail.com>
2024-05-15 00:15:47 -0300

    feat: criando router tams e adicioando router no app

---
+++ app/backend/src/routes/teamsRoutes.ts
@@ -0,0 +1,15 @@
+import { Request, Response, Router } from 'express';
+import TeamsController from '../Controller/TeamsController';
+
+const teamsController = new TeamsController();
+const router = Router();
+
+router.get('/', (req: Request, res: Response) => {
+  teamsController.getAllTeams(req, res);
+});
+
+router.get('/:id', (req: Request, res: Response) => {
+  teamsController.getTeamById(req, res);
+});
+
+export default router;
