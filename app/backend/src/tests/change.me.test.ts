1b15763bdf6f00fc127d208e5b8e90946894f64a
Gabriel Amorim <ricksamann@gmail.com>
2024-05-15 00:14:23 -0300

    feat: Criando os testes e mocks da rota teams

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -4,11 +4,13 @@ import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
-import Example from '../database/models/ExampleModel';
+import SequelizeTeams from '../database/models/teams.model';
 
 import { Response } from 'superagent';
+import { AllTeamsMock } from './mocks';
 
 chai.use(chaiHttp);
+
 
 const { expect } = chai;
 
@@ -31,15 +33,28 @@ describe('Seu teste', () => {
   //   (Example.findOne as sinon.SinonStub).restore();
   // })
 
-  // it('...', async () => {
-  //   chaiHttpResponse = await chai
-  //      .request(app)
-  //      ...
+  it('Testando getAllTeams', async () => {
+    sinon.stub(SequelizeTeams, 'findAll').resolves(AllTeamsMock as any);
 
-  //   expect(...)
-  // });
+    const chaiHttpResponse = await chai
+       .request(app)
+       .get('/teams');
 
-  it('Seu sub-teste', () => {
-    expect(false).to.be.eq(true);
+    expect(chaiHttpResponse.status).to.be.eq(200);
+    expect(chaiHttpResponse.body).to.be.deep.eq(AllTeamsMock);
   });
+  it('Testando getAllTeams', async () => {
+    sinon.stub(SequelizeTeams, 'findByPk').resolves({id: 1, teamName: 'Time 1'} as any);
+
+    const chaiHttpResponse = await chai
+       .request(app)
+       .get('/teams/1');
+
+    expect(chaiHttpResponse.status).to.be.eq(200);
+    expect(chaiHttpResponse.body).to.be.deep.eq({id: 1, teamName: 'Time 1'});
+  });
+
+  /*it('Seu sub-teste', () => {
+    expect(false).to.be.eq(true);
+  });*/
 });
